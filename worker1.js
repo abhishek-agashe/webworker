917df6ae24ae49ca9cdd1e87e91c04b3be1aed41
Abhishek Agashe <abhishek.agashe@gmail.com>
2016-07-02 12:12:37 -0700

    Update worker1.js

--- worker1.js
+++ worker1.js
@@ -1,6 +1,7 @@
 (function(){
   self.addEventListener('message', function(e) {
       self.postMessage({'msg': 'got it', 'data': 'workers/I am awesome!!'});
+      console.log('DATA from main to eorker' + e.data);
       if(e.data.msg === 'error') {
         throw new Error('an error occured in worker1...');
       }
