8f63c6c733c9b76ecb22044fbeddb54d2d0f0209
Abhishek Agashe <abhishek.agashe@gmail.com>
2016-07-02 12:07:19 -0700

    Update worker1.js

--- worker1.js
+++ worker1.js
@@ -1,5 +1,8 @@
 (function(){
   self.addEventListener('message', function(e) {
       self.postMessage({'msg': 'got it', 'data': 'workers/I am awesome!!'});
+      if(e.data.msg === 'error') {
+        throw new Error('an error occured in worker1...');
+      }
   });
 })();
