4710b2e9e3d75fa235729024595639a83ed6bdba
Abhishek Agashe <abhishek.agashe@gmail.com>
2016-07-02 12:05:19 -0700

    Update main.js

--- main.js
+++ main.js
@@ -2,19 +2,39 @@
   
 
   var startBtn = document.getElementById('start-btn');
+  var errorBtn = document.getElementById('error-btn');
+  var terminateBtn = document.getElementById('terminate-btn');
+  var worker = new Worker('worker1.js');
   startBtn.addEventListener('mouseup', function(e) {
-    talkToWorker();
+    talkToWorker(worker);
   });
- 
+ startBtn.addEventListener('mouseup', fuunction(e) {
+    handleError(worker);
+ });
+ terminateBtn.addEventListener('mouseup', function(e){
+    stopWorker(worker);
+ });
 })();
 
-function talkToWorker() {
+function talkToWorker(worker) {
 
-  var worker = new Worker('worker1.js');
+  
   var resultElm = document.getElementById('result');
   worker.postMessage({msg: 'dude', data:'I am main thread'});
   worker.addEventListener('message', function(e){
     console.log('data coming from wroker', e.data);
     resultElm.inerHTML = e.data.message;  
   });
+}
+
+function handleError(worker) {
+  var errorElm = document.getElementById('error');
+  worker.postMessage('error', function(e){
+      console.log(e.data);
+      errorElm.textContent =  e.data;
+  });
+}
+
+function stopWorker(worker) {
+  worker.terminate();
 }
