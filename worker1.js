25277873c79dfa7bbf3793cb55e214082ccb87ea
Abhishek Agashe <abhishek.agashe@gmail.com>
2016-07-05 20:23:12 -0700

    Update worker1.js

--- worker1.js
+++ worker1.js
@@ -1,7 +1,7 @@
 (function(){
   self.addEventListener('message', function(e) {
       console.log('DATA from main to eorker' + JSON.stringify(e.data));
-      window.localStorage.setItem('foo','Abhishek');
+      self.window.localStorage.setItem('foo','Abhishek');
       self.postMessage({'msg': 'got it', 'data': 'workers/I am awesome!!'});
       
       var x = s + 23;
