78ae392b557d733206fa68b4a96eab7875049a4d
Abhishek Agashe <abhishek.agashe@gmail.com>
2016-07-16 16:24:06 -0700

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
   var errorBtn = document.getElementById('error-btn');
   var terminateBtn = document.getElementById('terminate-btn');
   var worker = new Worker('worker1.js');
-  fetch('http://i.imgur.com/NruwA2a.gif').then(function(response){
+  fetch('https://i.imgur.com/NruwA2a.gif').then(function(response){
     return response.json();
   }).then(function(data){
     console.log(data);
