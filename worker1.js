f15282acea5e3f0d36bae3b85a34add7b2fe1589
Abhishek Agashe <abhishek.agashe@gmail.com>
2016-07-16 16:40:01 -0700

    Update worker1.js

--- worker1.js
+++ worker1.js
@@ -5,8 +5,9 @@
       
   //     var x = s + 23;
   // });
+  console.log('Wroker1 is ready to respond');
   self.addEventListener('fetch', function(e) {
-      
+      console.log('worker1 heard the fetch event');
       e.respondWith(new Response('hello am I am worker'));
   });
 })();
